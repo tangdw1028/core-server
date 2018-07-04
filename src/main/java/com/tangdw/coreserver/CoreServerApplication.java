60998843c42381e07697619975d4ab8c4e220188
tangdw <690351848@qq.com>
2018-07-04 16:33:51 +0800

    first commit

---
+++ src/main/java/com/tangdw/coreserver/CoreServerApplication.java
@@ -0,0 +1,12 @@
+package com.tangdw.coreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreServerApplication.class, args);
+    }
+}
